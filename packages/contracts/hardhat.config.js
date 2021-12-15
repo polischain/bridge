62b3825b0c64d02b1b9606671cc001250f25982c
eabz <eabz@polispay.org>
2021-12-15 13:37:26 -0600

    Init

---
+++ packages/contracts/hardhat.config.js
@@ -0,0 +1,26 @@
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+    networks: {
+        hardhat: {},
+    },
+    solidity: {
+        compilers: [{
+            version: "0.4.24",
+            settings: {
+                optimizer: {
+                    enabled: true,
+                    runs: 200
+                }
+            },
+        },{
+            version: "0.7.5",
+            settings: {
+                optimizer: {
+                    enabled: true,
+                    runs: 200
+                }
+            },
+        }],
+    }
+};
