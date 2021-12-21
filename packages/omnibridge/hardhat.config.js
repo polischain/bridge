b7aed26f8a7c0a30c4e3cfe9ab65dde39731969a
eabz <eabz@polispay.org>
2021-12-21 10:50:51 -0600

    Base files for amb-bridge

---
+++ packages/omnibridge/hardhat.config.js
@@ -0,0 +1,29 @@
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  networks: {
+    hardhat: {},
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.4.24",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.7.5",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+};
